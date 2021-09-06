5268583fc54d01c6cd5d6b4f7da415622380b25a
Paulus Siahaan <siahaan.paulus123@gmail.com>
2021-09-06 09:10:31 +0700

    Create Simple routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
-import Login from "./pages/Login";
+import Login from "./pages/member/login/Login";
 import React from "react";
-import SideBar from "./pages/sidebar/SideBar";
-import Register from "./pages/register/Register";
-import Home from "./pages/home/home";
-
+import SideBar from "./pages/admin/sidebar/SideBar";
+import Register from "./pages/member/register/Register";
+import Home from "./pages/member/home/home";
+import Dashboard from "./pages/member/dashboard/dashboard";
+import Project from "./pages/member/project/project";
+import MyArticles from "./pages/member/myarticles/myarticles";
+import CreateProject from "./pages/member/createproject/createproject";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Enrollment from "./pages/admin/enrollment/Enrollment";
 function App() {
   return (
     <>
-      <Login />
-      <SideBar />
-      <Register />
-      {/* <Home /> */}
+      <Router>
+        <Switch>
+          <Route path="/register" component={Register} />
+          <Route path="/" exact component={Login} />
+          <Route path="/member/beranda-artikel">
+            <Dashboard>
+              <MyArticles />
+            </Dashboard>
+          </Route>
+          <Route path="/member/beranda-project">
+            <Dashboard> 
+              <Home />
+            </Dashboard>
+          </Route>
+          <Route path="/member/project-saya">
+            <Dashboard> 
+              <Project />
+            </Dashboard>
+          </Route>
+          <Route path="/member/buat-project">
+            <Dashboard>
+              <CreateProject />
+            </Dashboard>
+          </Route>
+          <Route path="/home">
+            <SideBar>
+              <Home />
+            </SideBar>
+          </Route>
+          <Route path="/beranda-admin">
+            <SideBar>
+              <Enrollment />
+            </SideBar>
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
